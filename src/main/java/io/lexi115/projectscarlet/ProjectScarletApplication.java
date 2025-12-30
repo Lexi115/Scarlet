61c311cfc176aecd56270da561d22b6c2e8f864a
Alessio <83905118+Lexi115@users.noreply.github.com>
2025-12-30 21:27:16 +0100

    (WIP) Added GitHub Actions workflow file
    Signed-off-by: Alessio <83905118+Lexi115@users.noreply.github.com>

--- src/main/java/io/lexi115/projectscarlet/ProjectScarletApplication.java
+++ src/main/java/io/lexi115/projectscarlet/ProjectScarletApplication.java
@@ -3,10 +3,13 @@ package io.lexi115.projectscarlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main class.
+ */
 @SpringBootApplication
 public class ProjectScarletApplication {
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         SpringApplication.run(ProjectScarletApplication.class, args);
     }
 
