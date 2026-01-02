4362b28b3f101ee3efa0d27b98c0aa7d473de563
Alessio <83905118+Lexi115@users.noreply.github.com>
2026-01-02 23:10:32 +0100

    Added job that deletes old refresh tokens from DB
    Signed-off-by: Alessio <83905118+Lexi115@users.noreply.github.com>

--- src/main/java/io/lexi115/projectscarlet/core/SchedulerConfig.java
+++ src/main/java/io/lexi115/projectscarlet/core/SchedulerConfig.java
@@ -1,12 +1,17 @@
 package io.lexi115.projectscarlet.core;
 
+import io.lexi115.projectscarlet.auth.jwt.RefreshTokenService;
 import io.lexi115.projectscarlet.words.WordService;
-import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 
 /**
  * Configuration class for scheduling asynchronous jobs.
@@ -16,7 +21,6 @@ import org.springframework.scheduling.annotation.Scheduled;
  */
 @Configuration
 @EnableScheduling
-@AllArgsConstructor
 public class SchedulerConfig {
     /**
      * The logger.
@@ -29,6 +33,22 @@ public class SchedulerConfig {
     private final WordService wordService;
 
     /**
+     * The refresh token service.
+     */
+    private final RefreshTokenService refreshTokenService;
+
+    /**
+     * Refresh token's lifecycle (in seconds).
+     */
+    @Value("${scheduler.jobs.remove-old-refresh-tokens.refresh-token-lifecycle}")
+    private Long refreshTokenLifecycle;
+
+    public SchedulerConfig(final WordService wordService, final RefreshTokenService refreshTokenService) {
+        this.wordService = wordService;
+        this.refreshTokenService = refreshTokenService;
+    }
+
+    /**
      * Job that chooses a random word periodically.
      *
      * @since 1.0
@@ -37,5 +57,18 @@ public class SchedulerConfig {
     public void chooseDailyWord() {
         var word = wordService.chooseRandomWord();
         LOGGER.info("Chosen daily word: {}", word);
+    }
+
+    /**
+     * Job that removes old refresh tokens from the database to clear up space.
+     *
+     * @since 1.0
+     */
+    @Scheduled(cron = "${scheduler.jobs.remove-old-refresh-tokens.cron}")
+    public void removeOldRefreshTokens() {
+        var dateLimit = LocalDateTime.ofEpochSecond(
+                Instant.now().minusSeconds(refreshTokenLifecycle).getEpochSecond(), 0, ZoneOffset.UTC);
+        refreshTokenService.removeAllRefreshTokensBefore(dateLimit);
+        LOGGER.info("Removed old refresh tokens!");
     }
 }
